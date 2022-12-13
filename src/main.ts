fa7cf6e657ad0d3b9eb0f8c75827c6950d511ccd
Nathaniel Quansah <n.bongo40@gmail.com>
2022-12-13 14:31:36 +0000

    initialized prisma, created incident model, added app config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // enable cors
+  app.enableCors();
+
+  // get app url
+  const serverUrl = await app.getUrl();
   await app.listen(3000);
+
+  console.log(`Application is running on: ${serverUrl}`);
 }
 bootstrap();
